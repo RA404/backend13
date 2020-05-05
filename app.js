f185f1d3274b3ea6b685d6001e92aa1c8c69e2d2
Repin Andrey <RA404@yandex.ru>
2020-05-05 12:45:49 +0500

    refactoring the project structure and connected to mestodb

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const router = require('./routes');
+const users = require('./routes/users.js');
+const cards = require('./routes/cards.js');
 
-const { PORT = 3000 } = process.env;
+const { PORT, DATABASE_URL } = require('./config.js');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', router);
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-// Handling 404
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', users);
+app.use('/cards', cards);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App listening ${PORT}`);
-});
+app.listen(PORT);
---
+++ config.js
@@ -0,0 +1,2 @@
+module.exports.PORT = process.env || 3000;
+module.exports.DATABASE_URL = 'mongodb://localhost:27017/mestodb';
