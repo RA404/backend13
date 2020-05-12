e60b65d676915694b71422f001ec244a5b49a5af
Repin Andrey <RA404@yandex.ru>
2020-05-12 21:59:09 +0500

    fixed port problem

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// module.exports.PORT = process.env || 3000;
-module.exports.PORT = 3000;
+module.exports.PORT = process.env.PORT || 3000;
 module.exports.DATABASE_URL = 'mongodb://localhost:27017/mestodb';
